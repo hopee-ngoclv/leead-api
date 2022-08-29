570dbd57b9f3028a679c14537aed1cc4a3466154
Ngoc Le <ngoc@hopee.com.vn>
2022-08-29 14:21:35 +0700

    test sh file

--- schema/bundle_netlify.sh
+++ schema/bundle_netlify.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Setup
+echo "1" 
+
 yarn global add https-proxy-agent
 yarn global add redoc-cli
-
+echo "2" 
 cd ./schema
 
+
+echo "3" 
 # Bundle OpenAPI to HTML
 redoc-cli bundle leead-web-app.yaml -o index.html
